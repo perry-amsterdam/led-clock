9ab0b340b038fee027593df9e72083847ee4e2d9
Perry Couprie <perry@couprie.net>
2019-05-02 00:30:50 +0200

    Added sketch for auto connect to wifi using table or phone.

---
+++ arduino/AutoConnect/AutoConnect.ino
@@ -0,0 +1,26 @@
+#include <ESP8266WiFi.h>          // Replace with WiFi.h for ESP32
+#include <ESP8266WebServer.h>     // Replace with WebServer.h for ESP32
+#include <AutoConnect.h>
+
+ESP8266WebServer Server;          // Replace with WebServer for ESP32
+AutoConnect      Portal(Server);
+
+void rootPage() {
+  char content[] = "Hello, world";
+  Server.send(200, "text/plain", content);
+}
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  Serial.println();
+
+  Server.on("/", rootPage);
+  if (Portal.begin()) {
+    Serial.println("WiFi connected: " + WiFi.localIP().toString());
+  }
+}
+
+void loop() {
+    Portal.handleClient();
+}
