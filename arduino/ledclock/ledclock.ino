28047a8c54014666893e4225ca1dae5867b207c4
perry-amsterdam <18102268+perry-amsterdam@users.noreply.github.com>
2025-09-15 23:28:17 +0200

    Fixed compile

--- arduino/ledclock/ledclock.ino
+++ arduino/ledclock/ledclock.ino
@@ -8,8 +8,6 @@
 #include "wifi.h"
 #include "net_time.h"
 #include "portal.h"
-#include "ws2812b.h"
-#include <ESPmDNS.h>
 
 void setup()
 {
@@ -31,23 +29,12 @@ void setup()
 		if(setupTimeFromInternet(/*acceptAllHttps=*/true))
 		{
 			lastPrintMs = millis();
-			g_timeReady = true;
+      g_timeReady = true;
 		}
 		else
 		{
 			Serial.println("[Boot] NTP/TZ setup mislukt");
 		}
-
-		-
-			if (MDNS.begin("ledclock"))
-		{
-			Serial.println("mDNS responder started: http://ledclock.local");
-		}
-		else
-		{
-			Serial.println("Error setting up MDNS responder!");
-		}
-
 	}
 	else
 	{
@@ -79,7 +66,7 @@ void loop()
 				time_t epoch = time(nullptr);
 				Serial.printf("\r[Time] %s | epoch=%ld | TZ=%s | CC=%s\n", buf, (long)epoch, g_timezoneIANA.c_str(), g_countryCode.length()? g_countryCode.c_str():"(?)");
 
-				// Hier komt de code voor de ws2812b leds.
+        // Hier komt de code voor de ws2812b leds.
 
 			}
 			else
