75b342c1bc6c22709651e12a27c98a81f03d7606
perry-amsterdam <18102268+perry-amsterdam@users.noreply.github.com>
2025-09-22 18:33:46 +0200

    update

--- arduino/ledclock-freertos/task_wifi.cpp
+++ arduino/ledclock-freertos/task_wifi.cpp
@@ -21,40 +21,43 @@ static void push_led(uint8_t r,uint8_t g,uint8_t b,uint16_t ms)
 
 void task_wifi(void*)
 {
-	Preferences prefs;
-	// Load saved credentials
-	prefs.begin(PREF_NS,true);
-	String ssid = prefs.getString("ssid","");
-	String pass = prefs.getString("pass","");
-	prefs.end();
-
-	if(ssid.isEmpty())
-	{
-		// Start portal immediately
-		xEventGroupSetBits(g_sysEvents, EVT_PORTAL_ON);
-		startPortal();
-	}
-
-	for(;;)
-	{
-		if(!rtos_test_bits(EVT_WIFI_UP) && !rtos_test_bits(EVT_PORTAL_ON))
-		{
-			push_led(0,0,32,50);
-			connectWiFi(ssid, pass);
-			// Wait up to 10s
-			uint32_t t0 = hal_millis();
-			while(hal_millis()-t0 < 10000 && WiFi.status()!=WL_CONNECTED){ hal_delay_ms(50); }
-			if(WiFi.status()==WL_CONNECTED)
-			{
-				xEventGroupSetBits(g_sysEvents, EVT_WIFI_UP);
-			}
-			else
-			{
-				// Fallback to portal
-				xEventGroupSetBits(g_sysEvents, EVT_PORTAL_ON);
-				startPortal();
-			}
-		}
+//
+//	Preferences prefs;
+//	// Load saved credentials
+//	prefs.begin(PREF_NS,true);
+//	String ssid = prefs.getString("ssid","");
+//	String pass = prefs.getString("pass","");
+//	prefs.end();
+//
+//	if(ssid.isEmpty())
+//	{
+//		// Start portal immediately
+//		xEventGroupSetBits(g_sysEvents, EVT_PORTAL_ON);
+//		startPortal();
+//	}
+//
+//	for(;;)
+//	{
+//		if(!rtos_test_bits(EVT_WIFI_UP) && !rtos_test_bits(EVT_PORTAL_ON))
+//		{
+//			push_led(0,0,32,50);
+//			connectWiFi(ssid, pass);
+//			// Wait up to 10s
+//			uint32_t t0 = hal_millis();
+//			while(hal_millis()-t0 < 10000 && WiFi.status()!=WL_CONNECTED){ hal_delay_ms(50); }
+//			if(WiFi.status()==WL_CONNECTED)
+//			{
+//				xEventGroupSetBits(g_sysEvents, EVT_WIFI_UP);
+//			}
+//			else
+//			{
+//				// Fallback to portal
+//				xEventGroupSetBits(g_sysEvents, EVT_PORTAL_ON);
+//				startPortal();
+//			}
+//		}
+//		hal_delay_ms(250);
+//	}
+//
 		hal_delay_ms(250);
-	}
 }
