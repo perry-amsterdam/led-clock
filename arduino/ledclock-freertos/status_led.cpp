e1a241daf1f81c76975dc3c7b2671e2e8afdfbcf
perry-amsterdam <18102268+perry-amsterdam@users.noreply.github.com>
2025-09-23 15:14:27 +0200

    update

--- arduino/ledclock-freertos/status_led.cpp
+++ arduino/ledclock-freertos/status_led.cpp
@@ -2,7 +2,9 @@
 #include "status_led.h"
 
 void ledBegin(){ pixel.begin(); pixel.setBrightness(LED_BRIGHTNESS); pixel.clear(); pixel.show(); }
+
 void ledColor(uint8_t r,uint8_t g,uint8_t b){ pixel.setPixelColor(0, pixel.Color(r,g,b)); pixel.show(); }
+
 void ledBlue(){   if(DEBUG_NET) Serial.println("\r[LED] Blauw (verbinden)\n\r");    ledColor(0,0,60); }
 void ledRed(){    if(DEBUG_NET) Serial.println("\r[LED] Rood (geen WiFi/AP)\n\r");  ledColor(60,0,0); }
 void ledGreen(){  if(DEBUG_NET) Serial.println("\r[LED] Groen (verbonden)\n\r");    ledColor(0,60,0); }
