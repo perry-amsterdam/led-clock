b36ffe2a3c5ec72f3662d5723ca2c0771e854b9f
perry-amsterdam <18102268+perry-amsterdam@users.noreply.github.com>
2025-10-01 19:29:59 +0200

    removed old code

--- arduino/ledclock/status_led.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "globals.h"
-
-void ledBegin();
-void ledColor(uint8_t r,uint8_t g,uint8_t b);
-void ledBlue();
-void ledRed();
-void ledGreen();
-void ledOff();
--- arduino/ledclock/wifi.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <Arduino.h>
-#include "globals.h"
-
-bool connectWiFi(const String& ssid, const String& pass, uint32_t timeoutMs = 15000);
