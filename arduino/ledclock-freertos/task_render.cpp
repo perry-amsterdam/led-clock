8065e8239a7dd1b6b94f14641d89a22dd054f3b9
perry-amsterdam <18102268+perry-amsterdam@users.noreply.github.com>
2025-10-02 23:08:34 +0200

    update

--- arduino/ledclock-freertos/task_render.cpp
+++ arduino/ledclock-freertos/task_render.cpp
@@ -21,17 +21,17 @@ void task_render(void*)
 		if (bits & EVT_PORTAL_ON)
 		{
 			// Captive portal actief  rood pulse
-			drawStatusTicks_Rotated_Static(100, 0, 0);
+			drawStatusTicks_Rotated_Static(50, 0, 0);
 		}
 		else if (!(bits & EVT_WIFI_UP))
 		{
 			// WiFi is niet verbonden  blauw pulse
-			drawStatusTicks_Rotated_Static(0, 0, 100);
+			drawStatusTicks_Rotated_Static(0, 0, 50);
 		}
 		else if (!(bits & EVT_TIME_READY))
 		{
 			// WiFi verbonden, maar tijd nog niet gesynchroniseerd  groen pulse
-			drawStatusTicks_Rotated_Static(0, 100, 0);
+			drawStatusTicks_Rotated_Static(0, 50, 0);
 		}
 
 		hal_delay_ms(500);
