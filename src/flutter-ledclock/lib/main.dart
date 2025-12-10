3273fceb54adb274a9a11599b6de26cf67de1da0
perry-amsterdam <18102268+perry-amsterdam@users.noreply.github.com>
2025-12-10 18:15:38 +0100

    update

--- src/flutter-ledclock/lib/main.dart
+++ src/flutter-ledclock/lib/main.dart
@@ -69,7 +69,7 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ],
           ),
-          body: LedClockControlPanel(host: host),
+          body: SafeArea(child: LedClockControlPanel(host: host)),
         );
       },
     );
